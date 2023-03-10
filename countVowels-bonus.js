f6c84495fb30c2fe111949779665b6d639a49471
Flux Feint <alexandernhell69@gmail.com>
2023-03-10 09:13:43 +0000

    He re-hecho el codigo de countVowels para tener un solo bucle anidado y escalable

--- countVowels-bonus.js
+++ countVowels-bonus.js
@@ -1,33 +1,32 @@
 function countVowels(word, anotherWord) {
-    //Ponemos todas las vocales que queremos que cuenten
-    const vowels = "aeiouAEIOUáéíóúÁÉÍÓÚàèìòùÀÈÌÒÙäëïöüÄËÏÖÜâêîôûÂÊÎÔÛ"; //He añadido vocales con varios acentos para que no deje de contarlas
-    //Iniciamos una variable que lleva la cuenta de las vocales
-    let vowelCount = 0;
-    
-    //Este bucle for recorre todo el string
-    for (let i = 0; i < word.length; i++) {
-    //La letra que está comprobando se comprueba si está en nuestra lista de vocales "vowels"
-      if (vowels.includes(word[i])) {
-            //En el caso de ser así la variable suma +1
-        vowelCount++;
-      }
-    }
 
-    //Ya que estamos manipulando la misma variable simplemente copiaré el mismo bucle for cambiando "word" por "anotherWord"
-    //Ya que si encuentra vocales en la segunda palabra las añadirá a la misma variable evitando tener que sumarlas luego
-    for (let i = 0; i < anotherWord.length; i++) {
-    //De nuevo la letra que está comprobando el bucle se comprueba si está en nuestra lista de vocales "vowels"
-        if (vowels.includes(anotherWord[i])) {
-            //En el caso de ser así la variable suma +1 de nuevo
-        vowelCount++;
-      }
-    }
+  //Ponemos todas las vocales que queremos contar en un string
+  const vowels = "aeiouAEIOUáéíóúÁÉÍÓÚàèìòùÀÈÌÒÙäëïöüÄËÏÖÜâêîôûÂÊÎÔÛ"; //He añadido vocales con varios acentos para que no deje de contarlas
+  //Iniciamos una variable que lleva la cuenta de las vocales
+  let vowelCount = 0;
 
-    //Con el méotodo return nos devuelve todas las vocales
+    /* Este bucle for recorre todos los argumentos de la función (en este caso word, anotherWord)
+    Usando este código lo hacemos facilmente escalable ya que podemos añadir argumentos
+    ad infinitum y siempre nos va a recorrer el length (la cantidad de argumentos) */
+    for (let k = 0; k < arguments.length; k++)
+    {
+      // Guardamos el input actual en una variable llamada "palabra"
+      palabra=arguments[k]
+      //Este bucle for recorre todo el string de la palabra actual letra por letra
+      for (let i = 0; i < palabra.length; i++) {
+      //La letra actual del bucle se comprueba si está en nuestra lista de vocales "vowels"
+      if (vowels.includes(palabra[i])) {
+      //En el caso de ser así la variable suma +1
+      vowelCount++;
+        }
+
+      }
+      /* Alexander recuerda tristemente como se tiró dos horas preguntandose por qué falla el código
+      habiendo puesto el return en el bucle anterior */
+    }
+    // Este return nos devuelve el número total de vocales
     return vowelCount;
-}
+  }
 
-console.log(countVowels("calor", "sal")); // 3
-console.log(countVowels("la", "le")); // 2
-console.log(countVowels("crwth", "crwth")); // 0 https://en.wikipedia.org/wiki/Crwth
-console.log(countVowels("Óscar", "âcèntöTèst")) // 6
+  console.log(countVowels("calor", "solano")); // Nos devuelve 5
+  console.log(countVowels("palabras","aleatorias","para","comprobar","que","el","codigo","escala","hermano")) // Nos devuelve 26
